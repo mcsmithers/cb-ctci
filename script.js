324d9c05ceb3d8c27c6862cdf19d0ebc15ca50a9
Christina Smithers <christina.smithers@gmail.com>
2018-11-28 12:17:54 -0800

    Solved the challenge
    The challenge was to see if a string was a permutation of another.  I solved it finally.  Any steps after this would be to make it a much more efficient solution.

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
- function getPermutations(myStr) {
-      const results = [];
+function getPermutations(myStr) {
+  const firstPermutationResults = [];
 
-      if (myStr.length === 1) 
-      {
-        results.push(myStr);
-        return results;
-      }
+  if (myStr.length === 1) {
+    firstPermutationResults.push(myStr);
+    return firstPermutationResults;
+  }
 
-      for (let char = 0; char < myStr.length; char++) 
-      {
-        const firstLetter = myStr[char];
-        const otherLetter = myStr.substring(0, char) + myStr.substring(char + 1);
-        const otherPermutations = getPermutations(otherLetter);
-        
-        for (let anotherChar = 0; anotherChar < otherPermutations.length; anotherChar++) {
-          results.push(firstLetter + otherPermutations[anotherChar]);
-        }
-      }
-      return results;
+  for (let char = 0; char < myStr.length; char++) {
+    // so we're first forming the words with a starting point and then recursively building
+    const firstLetter = myStr[char];
+    const otherLetter = myStr.substring(0, char) + myStr.substring(char + 1);
+    const otherPermutations = getPermutations(otherLetter);
+
+    for (let anotherChar = 0; anotherChar < otherPermutations.length; anotherChar++) {
+      firstPermutationResults.push(firstLetter + otherPermutations[anotherChar]);
     }
-    
-    const permutation = getPermutations('dog', 'god');
-    console.log(permutation);
 
-    // Next step: see if two strings are permutations of each other
+  }
+  return firstPermutationResults;
+}
 
+const permutation = getPermutations('dog');
+console.log(permutation);
 
+// Time to compare
+const otherStr = 'god';
+
+const iterator = permutation.values();
+
+// Yay, some es 6
+for (const value of iterator) {
+  if (otherStr == value) {
+    console.log('Match! "' + value + '" is the same as "' + otherStr + '"');
+  }
+  else {
+    console.log(otherStr + ' is not a match for ' + value);
+  }
+}
